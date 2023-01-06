91153ff20c37be31fc5146edebb25714fbbbe317
alexfortin <fortin.alex14@gmail.com>
2023-01-06 02:12:31 -0600

    created the type definitions and the resolvers

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+//import apollo server and typeDefs and resolvers
+const {ApolloServer} = require("apollo-server-express")
+const {typeDefs, resolvers} = require("./schemas")
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+//create apollo server and pass schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+//create instance of apollo server with graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start()
+
+  //integrate apollo server with the express application as middleware
+  server.applyMiddleware({app})
+}
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+
+    console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`)
+  });
+});
+
+//call async functions to start the server
+startApolloServer(typeDefs, resolvers)
